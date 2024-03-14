68facc6e4db0c94917bd8c42ef3292982e0f3a6d
bvogler10 <brinjavogler@gmail.com>
2024-03-14 17:21:58 -0400

    successfully saves image and text from previous state

--- app/src/main/java/com/example/maintainingsharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/maintainingsharedpreferences/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.maintainingsharedpreferences
 import android.content.SharedPreferences
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -18,27 +19,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("ActivityLifeCycle", "onCreate")
         val button = findViewById<Button>(R.id.loadImageButton)
         image = findViewById(R.id.savedImage)
         userText = findViewById(R.id.savedText)
 
         sharedPreferences = getSharedPreferences("ImageAndText", MODE_PRIVATE);
 
-//        loadSavedImage()
-//        loadSavedText()
+        loadSavedImage()
+        loadSavedText()
 
         button.setOnClickListener {
             loadImage()
+            saveImage()
+            saveText()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // Save state
-//        saveImageToPreferences()
-//        saveTextToPreferences()
+        Log.d("ActivityLifeCycle", "onDestroy")
     }
 
+    private fun saveText() {
+        val editor = sharedPreferences.edit()
+        editor.putString("text", userText.text.toString())
+        editor.apply()
+    }
+
+    private fun saveImage() {
+        val editor = sharedPreferences.edit()
+        editor.putInt("image", image.drawable?.let { image.tag as? Int ?: -1 } ?: -1)
+        editor.apply()
+    }
 
 
     private fun loadImage() {
@@ -46,14 +59,20 @@ class MainActivity : AppCompatActivity() {
         val resId = resources.getIdentifier("drawable_$random", "drawable", packageName)
         val drawable: Drawable? = resources.getDrawable(resId, null)
         image.setImageDrawable(drawable)
+        image.tag = random
     }
 
     private fun loadSavedText() {
-        TODO("Not yet implemented")
-    }
+        val text = sharedPreferences.getString("text", "")
+        userText.setText(text)    }
 
     private fun loadSavedImage() {
-        TODO("Not yet implemented")
+        val imageNum = sharedPreferences.getInt("image", -1)
+        val imageId = "drawable_$imageNum"
+        if (imageNum != -1) {
+            val resId = resources.getIdentifier(imageId, "drawable", packageName)
+            val drawable: Drawable? = resources.getDrawable(resId, null)
+            image.setImageDrawable(drawable)
+        }
     }
-
 }
